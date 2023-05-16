23828a24bfce597aaf7efc414fb0189e5722ae3e
Alexander Høystad <alex980x@edu.zealand.dk>
2023-05-16 10:58:48 +0200

    Login gemmes med Cookies

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SoundScape_Tour_Guide_Website.Pages.Account
@@ -18,9 +20,31 @@ namespace SoundScape_Tour_Guide_Website.Pages.Account
            
         }
 
-        public void OnPost() 
-        { 
-        
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            if (!ModelState.IsValid) return Page();
+
+            // Verify the credential
+            if (Credential.Username == "admin" && Credential.Password == "password")
+
+            {
+                // Creating the security context 
+                var claims = new List<Claim> {
+
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                
+                
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
 
     }
@@ -28,11 +52,13 @@ namespace SoundScape_Tour_Guide_Website.Pages.Account
     public class Credential
     {
         [Required]
-        [Display(Name = "Username")]
+        [Display(Name = "User Name")]
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
     }
+
+
 
 
 }
