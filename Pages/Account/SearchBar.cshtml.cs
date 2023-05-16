8048b4d808a3c7b438755be7fd9153abac4caa6f
Nicolai Hiort Ackermann <nico31a5@edu.zealand.dk>
2023-05-16 10:30:12 +0200

    lavet søge felt og added flere sider i toppen

---
+++ Pages/Account/SearchBar.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoundScape_Tour_Guide_Website.Pages.Account
+{
+    public class SearchBarModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Shared/Lyt til lydfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoundScape_Tour_Guide_Website.Pages.Shared
+{
+    public class Lyt_til_lydfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
